bfb9e0181301d02db1465c5fce1a3fa3bb2e4efb
hikmatyar22 <abiq5161@gmail.com>
2024-05-04 20:59:56 +0700

    membuat switch dan beberapa case

--- PRAK9ALGO/PRAK9ALGO.cpp
+++ PRAK9ALGO/PRAK9ALGO.cpp
@@ -198,7 +198,36 @@ int main()
 			char ch;
 			cin >> ch;
 
-			
+			switch (ch)
+			{
+			case '1':
+				addNode();
+				break;
+			case '2':
+				deleteNode();
+				break;
+			case '3':
+				traverse();
+				break;
+			case '4':
+				revtraverse();
+				break;
+			case '5':
+				searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+
+			}
+		}
+		catch (exception& e)
+		{
+			cout << "Check for the values entered." << endl;
+		}
+	}
 }
 
 
